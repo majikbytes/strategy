4b02284a4506451397af25a4531a2efc1cdcdea3
Sufyan Adam <pair+sufyan@mavenlink.com>
2013-09-20 17:44:48 -0700

    Fri Sep 20 17:44:48 PDT 2013

--- strategy.rb
+++ strategy.rb
@@ -2,6 +2,42 @@ require 'aggressive'
 require 'defensive'
 require 'state'
 
+include Aggressive
+include Defensive
+
+@state = State.new
 on_turn do
-  act_aggressively
+  @state.update me, battle
+  turn = handle_turn
+  @state.save_turn turn
+  turn
+end
+
+def handle_turn
+  attacker = imminent_attacker
+  return dodge attacker if attacker
+
+  if my.ammo >= 2
+    enemy = choose_enemy
+    comp = calculate_comp enemy
+    turn = act_aggressively enemy, compensate: comp
+    @state.save_fire enemy, comp if turn =~ /^f/
+    return turn
+  end
+
+  act_defensively
+end
+
+def imminent_attacker
+  attackers = @state.imminent_attackers
+  return nil if attackers.empty?
+  attackers.shuffle[0]
+end
+
+def choose_enemy
+  opponents.first
+end
+
+def calculate_comp(enemy)
+  @state.calculate_comp enemy
 end
